48881bd12f9fb8eff01feb28b0789ebc6f5017bb
Jianmin <nicklee19950330@gmail.com>
2019-09-17 16:52:15 -0500

    Updates

---
+++ precache-manifest.2582a92140163ff2dcc04edec9cf982a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eacbb8067aa0d1d7aa1a1b4cc562b0e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ea58d5ecf45d826e9ff",
+    "url": "/monsters-rolodex/static/css/main.3240896b.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "2ea58d5ecf45d826e9ff",
+    "url": "/monsters-rolodex/static/js/main.a031c6ed.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
